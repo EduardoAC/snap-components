c70fe14c014fd2adf4e4b038f063da9e38c7e73a
Eduardo <EduardoAC@users.noreply.github.com>
2024-11-16 12:48:57 +0000

    Setup library mode with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SnapComponents", // UMD global variable if needed
+      formats: ["es", "cjs"], // Suitable for both web and mobile projects
+      fileName: (format) => `snap-components.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react-native"], // Mark peer dependencies
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react-native": "ReactNative",
+        },
+      },
+    },
+  },
+});
