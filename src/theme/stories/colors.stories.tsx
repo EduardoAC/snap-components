c15420ee854dba58989474a0c0df088064f7d747
Eduardo <EduardoAC@users.noreply.github.com>
2024-11-17 09:20:49 +0000

    Resolve linter issues after updating packages

--- src/theme/stories/colors.stories.tsx
+++ src/theme/stories/colors.stories.tsx
@@ -10,6 +10,7 @@ export default meta;
 
 export const Default: StoryObj = {
   render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const theme = useTheme();
     return (
       <div
